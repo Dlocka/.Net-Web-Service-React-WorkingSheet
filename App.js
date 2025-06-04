deaad5540f913d050e6131df2c2070d160c37a0b
Chonghao Li <1076516565@qq.com>
2025-06-04 11:18:26 +0100

    Vite

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Mybutton(){
+    return (<button>This is a button</button>);
+}
+
+export default function MyApp(){
+
+    return(<div>
+        <h1>welcome to my </h1>
+    </div>)
+}
